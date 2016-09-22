950100480a3d2103e350f97ecc7ebb3b493f0256
Richa <singh.richa123495@gmail.com>
2016-09-22 11:19:28 +0530

    Add files via upload

---
+++ Increase_youtube_views.py
@@ -0,0 +1,9 @@
+import time
+import webbrowser
+
+#how much views you want
+
+print("Time increased\n" +time.ctime())
+for count in range(30):
+    time.sleep(2) 
+    webbrowser.open("https://www.youtube.com/watch?x=id") #specify the id
