204cdb876fe7f27a78d23783685345ae3bec0ebf
Richa <singh.richa123495@gmail.com>
2016-09-22 11:52:37 +0530

    Update Increase_youtube_views.py

--- Increase_youtube_views.py
+++ Increase_youtube_views.py
@@ -3,7 +3,7 @@ import webbrowser
 
 #how much views you want
 
-print("Time increased\n" +time.ctime())
+print("Time increased,Enjoy!!\t\n" +time.ctime())
 for count in range(30):
-    time.sleep(2) 
+    time.sleep(3) 
     webbrowser.open("https://www.youtube.com/watch?x=id") #specify the id
